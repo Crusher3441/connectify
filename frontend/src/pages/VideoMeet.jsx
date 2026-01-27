dc372f45e0d01095380055fee46721704b93adbf
Crusher3441 <vishal.singhh.2003@gmail.com>
2026-01-27 23:46:42 +0530

    videoMeet.jsx updated

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -1,81 +1,81 @@
-import { Button, TextField } from "@mui/material";
-import React from "react";
-import { useRef, useState } from "react";
+// import { Button, TextField } from "@mui/material";
+// import React from "react";
+// import { useRef, useState } from "react";
 
-import io from "socket.io-client";
+// // import io from "socket.io-client";
 
-const server_url = "http://localhost:8000";
+// // const server_url = "http://localhost:8000";
 
-let connections = {};
+// // let connections = {};
 
-// io.connect("https://connectify-eight.vercel.app/");
+// // io.connect("https://connectify-eight.vercel.app/");
 
-let connect = () =>{
+// let connect = () =>{
   
-}
+// }
 
-const peerConfigConnections = {
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-};
+// const peerConfigConnections = {
+//   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+// };
 
-function VideoMeet() {
-  var socketRef = useRef();
-  let socketIdRef = useRef();
+// function VideoMeet() {
+//   var socketRef = useRef();
+//   let socketIdRef = useRef();
 
-  let localVideoref = useRef();
+//   let localVideoref = useRef();
 
-  let [videoAvailable, setVideoAvailable] = useState(true);
+//   let [videoAvailable, setVideoAvailable] = useState(true);
 
-  let [audioAvailable, setAudioAvailable] = useState(true);
+//   let [audioAvailable, setAudioAvailable] = useState(true);
 
-  let [video, setVideo] = useState([]);
+//   let [video, setVideo] = useState([]);
 
-  let [audio, setAudio] = useState();
+//   let [audio, setAudio] = useState();
 
-  let [screen, setScreen] = useState();
+//   let [screen, setScreen] = useState();
 
-  let [showModal, setModal] = useState(true);
+//   let [showModal, setModal] = useState(true);
 
-  let [screenAvailable, setScreenAvailable] = useState();
+//   let [screenAvailable, setScreenAvailable] = useState();
 
-  let [messages, setMessages] = useState([]);
+//   let [messages, setMessages] = useState([]);
 
-  let [message, setMessage] = useState("");
+//   let [message, setMessage] = useState("");
 
-  let [newMessages, setNewMessages] = useState(3);
+//   let [newMessages, setNewMessages] = useState(3);
 
-  let [askForUsername, setAskForUsername] = useState(true);
+//   let [askForUsername, setAskForUsername] = useState(true);
 
-  let [username, setUsername] = useState("");
+//   let [username, setUsername] = useState("");
 
-  const videoRef = useRef([]);
+//   const videoRef = useRef([]);
 
-  let [videos, setVideos] = useState([]);
-  return (
-    <div>
-      {askForUsername === true ? (
-        <div>
-          <h2>Enter into Lobby </h2>
-          <TextField
-            id="outlined-basic"
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            variant="outlined"
-          />
-          <Button variant="contained" onClick={connect}>
-            Connect
-          </Button>
+//   let [videos, setVideos] = useState([]);
+//   return (
+//     <div>
+//       {askForUsername === true ? (
+//         <div>
+//           <h2>Enter into Lobby </h2>
+//           <TextField
+//             id="outlined-basic"
+//             label="Username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//             variant="outlined"
+//           />
+//           <Button variant="contained" onClick={connect}>
+//             Connect
+//           </Button>
 
-          <div>
-            <video ref={localVideoref} autoPlay muted></video>
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
-    </div>
-  );
-}
+//           <div>
+//             <video ref={localVideoref} autoPlay muted></video>
+//           </div>
+//         </div>
+//       ) : (
+//         <></>
+//       )}
+//     </div>
+//   );
+// }
 
-export default VideoMeet;
+// export default VideoMeet;
